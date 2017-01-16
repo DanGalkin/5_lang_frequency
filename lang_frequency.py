6a4d4f31737778153bdd68a005f74560b4d46e44
DanGalkin <dangalkin@gmail.com>
2017-01-16 23:55:11 +0300

    Update land_frequency.py
    Added the code

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,30 @@
+import operator
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='utf-8') as text_file:
+        return text_file.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words_list = text.split()
+    words_count = {}
+    for item in words_list:
+        words_count[item] = words_list.count(item)
+    top_words_count = sorted(words_count.items(), key=operator.itemgetter(1),
+                             reverse=True)[:10]
+    print('Самые частые слова')
+    for item in top_words_count:
+        print('%s встречается %s раз(а)' % (item[0], item[1]))
+
+
+def clear_text_from_punctuation(text):
+    punctuation_list = list('1234567890!@#$%^&*(){}[];:",./<>?~_=|`\u2014\u002D\u005C\u0027')
+    for item in punctuation_list:
+        text = text.lower().replace(item, '')
+    return text
 
 
 if __name__ == '__main__':
-    pass
+    filepath = str(input('Введите путь до текстового файла:'))
+    get_most_frequent_words(clear_text_from_punctuation(load_data(filepath)))
