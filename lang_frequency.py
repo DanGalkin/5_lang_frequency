70517384476fbc45336a21f83815d57cd1be65a3
DanGalkin <dangalkin@gmail.com>
2018-11-09 21:44:12 +0300

    added the filepath as an attribute on run

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,18 +1,39 @@
-import operator
+import re
+import sys
 from collections import Counter
 
-def load_data(filepath):
+
+def load_text_data(filepath):
+    '''
+    str -> str
+    gets the filepath to the file and returns the text from it
+    '''
     with open(filepath, encoding='utf-8') as text_file:
         return text_file.read()
 
 
 def get_most_frequent_words(text):
-    words_list = re.findall('\w+', text.lower())
+    '''
+    str -> list
+    The function finds all words in a string
+    and return the list of 10 most common with their frequency
+    '''
+    words_list = re.findall(r'\w+', text.lower())
     return Counter(words_list).most_common(10)
 
 
 if __name__ == '__main__':
-    filepath = str(input('Введите путь до текстового файла:'))
-    top_words_count = get_most_frequent_words(load_data(filepath))
+    try:
+        text_filepath = sys.argv[1]
+        text = load_text_data(text_filepath)
+    except IndexError:
+        print('Не указан путь к файлу с текстом, программа завершит работу.')
+        sys.exit()
+    except FileNotFoundError:
+        print('Файл не найден, программа завершит работу.')
+        sys.exit()
+
+    top_words_count = get_most_frequent_words(text)
+    print('Самые часто встречающиеся слова в тексте:')
     for item in top_words_count:
-        print('%s встречается %s раз(а)' % (item[0], item[1]))
+        print('Слово {:^10} встречается {:5} раз(а)'.format(item[0], item[1]))
