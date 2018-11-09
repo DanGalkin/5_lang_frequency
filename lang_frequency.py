280716b8147f2c7fd1ddaea9f49b71031ef5e237
DanGalkin <dangalkin@gmail.com>
2018-11-09 21:59:38 +0300

    removed magic numbers, renamed vars to be more precise, deleted docstrings

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,28 +4,19 @@ from collections import Counter
 
 
 def load_text_data(filepath):
-    '''
-    str -> str
-    gets the filepath to the file and returns the text from it
-    '''
     with open(filepath, encoding='utf-8') as text_file:
         return text_file.read()
 
 
-def get_most_frequent_words(text):
-    '''
-    str -> list
-    The function finds all words in a string
-    and return the list of 10 most common with their frequency
-    '''
-    words_list = re.findall(r'\w+', text.lower())
-    return Counter(words_list).most_common(10)
+def get_most_frequent_words(text_string, most_common):
+    words_list = re.findall(r'\w+', text_string.lower())
+    return Counter(words_list).most_common(most_common)
 
 
 if __name__ == '__main__':
     try:
         text_filepath = sys.argv[1]
-        text = load_text_data(text_filepath)
+        text_string = load_text_data(text_filepath)
     except IndexError:
         print('Не указан путь к файлу с текстом, программа завершит работу.')
         sys.exit()
@@ -33,7 +24,8 @@ if __name__ == '__main__':
         print('Файл не найден, программа завершит работу.')
         sys.exit()
 
-    top_words_count = get_most_frequent_words(text)
+    most_common = 10
+    top_words_count = get_most_frequent_words(text_string, most_common)
     print('Самые часто встречающиеся слова в тексте:')
-    for item in top_words_count:
-        print('Слово {:^10} встречается {:5} раз(а)'.format(item[0], item[1]))
+    for word in top_words_count:
+        print('Слово {:^10} встречается {:5} раз(а)'.format(word[0], word[1]))
